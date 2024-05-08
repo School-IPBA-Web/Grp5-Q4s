aff2cc81be5d924e9b2af1bdf1b9efd125ecc616
School-IPBA-Web <167827710+School-IPBA-Web@users.noreply.github.com>
2024-05-08 09:07:07 +0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,11 @@
+//
+// document.getElementById("body").onscroll = function myFunction() {  
+//    var scrolltotop = document.scrollingElement.scrollTop;
+//    var target = document.getElementById("main1");
+//    var xvalue = "center";
+//    var factor = 0.5;
+//    var yvalue = scrolltotop * factor;
+//    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+//  }
+//
+// yeah im not doing this anymore
---
+++ warning.js
@@ -0,0 +1,1 @@
+alert("Hello! If your operating system does not use MacOS, please use our site on 90% of size using Ctrl + Scrollwheel.");
